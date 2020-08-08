2d88775707e939bee91c6a23176b2ae16ffcd40e
Jamie Keelen <j.keelen@mumms.com>
2020-08-08 10:07:20 -0500

    inital commit

--- public/js/init.js
+++ public/js/init.js
@@ -102,15 +102,6 @@
 /*	Modal Popup
 ------------------------------------------------------*/
 
-    $('.item-wrap a').magnificPopup({
-
-       type:'inline',
-       fixedContentPos: false,
-       removalDelay: 200,
-       showCloseBtn: false,
-       mainClass: 'mfp-fade'
-
-    });
 
     $(document).on('click', '.popup-modal-dismiss', function (e) {
     		e.preventDefault();
--- src/resumeData.js
+++ src/resumeData.js
@@ -1,104 +1,104 @@
 let resumeData = {
     "imagebaseurl":"https://rbhatia46.github.io/",
-    "name": "Rahul Bhatia",
-    "role": "Frontend Developer and Data Scientist",
-    "linkedinId":"Your LinkedIn Id",
+    "name": "Jamie Keelen",
+    "role": "Full Stack Developer",
+    "linkedinId":"jamiekeelen",
     "skypeid": "Your skypeid",
-    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "roleDescription": "I have a deep passion for creating amazing things. I specialize in all things JavaScript, mainly React and Node. When I'm motivated to get something done, I produce results very quickly.",
     "socialLinks":[
         {
           "name":"linkedin",
-          "url":"https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
+          "url":"https://www.linkedin.com/in/jamiekeelen/",
           "className":"fa fa-linkedin"
         },
         {
           "name":"github",
-          "url":"http://github.com/rbhatia46",
+          "url":"http://github.com/jamie7893",
           "className":"fa fa-github"
-        },
-        {
-          "name":"skype",
-          "url":"http://twitter.com/rbhatia46",
-          "className":"fa fa-twitter"
         }
       ],
-    "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
-    "address":"India",
-    "website":"https://rbhatia46.github.io",
+    "aboutme":"Currently I am working full time as a Full Stack Developer for a company. I've been there for 4+ years and I am ready to take the next step in my career. In 2016 I completed the Hack Reactor program, which was a 3 month, 6 days a week, 12+ hours a day, very intense deep dive into JavaScript and all the frameworks and technologies around it.",
+    "address":"jamie78933@gmail.com",
+    "website":"(504) 952-3690",
     "education":[
       {
-        "UniversityName":"The LNM Insitute of Information Technology",
-        "specialization":"Some specialization",
+        "UniversityName":"Operation Spark",
+        "specialization":"Hack Reactor",
         "MonthOfPassing":"Aug",
-        "YearOfPassing":"2020",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "UniversityName":"Some University",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Jan",
-        "YearOfPassing":"2018",
-        "Achievements":"Some Achievements"
+        "YearOfPassing":"2016"
       }
     ],
     "work":[
       {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
+        "CompanyName":"Mumms Software",
+        "specialization":"Full Stack Developer",
+        "MonthOfLeaving":"Current Job",
+        "YearOfLeaving":"",
+        "Achievements":"When I was first hired at Mumms, I quickly realized they were using old technologies EVERYWHERE. Development tasks were taking way longer than what they should have because of the older stuff. I started showing them all of the new things I had just learned and they were instantly sold, we started phasing out all of the old code and started pulling out functionality from these massive bulky slow plain JavaScript/jQuery apps and building micro-apps as react components that we could pass around and install into any of ours apps easily. By doing so we were able to improve the QOL of all of the developers, making the work environment much easier and quicker to work in, as well as get features out quicker and with less bugs!"
       },
       {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
+        "CompanyName":"The Refinery Haus",
+        "specialization":"Freelance React/Redux",
+        "MonthOfLeaving":"Feb",
+        "YearOfLeaving":"2020",
+        "Achievements":""
       }
     ],
     "skillsDescription":"Your skills here",
     "skills":[
       {
-        "skillname":"HTML5"
+        "skillname":"JavaScript"
       },
       {
-        "skillname":"CSS"
+        "skillname":"TypeScript"
       },
       {
-        "skillname":"Reactjs"
+        "skillname":"React"
+      },
+      {
+        "skillname":"Node"
+      },
+      {
+        "skillname":"SQL"
+      },
+      {
+        "skillname":"Linux"
+      },
+      {
+        "skillname":"Bash"
       }
     ],
     "portfolio":[
       {
-        "name":"project1",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
+        "name":"Hummingbird Clinical",
+        "description":"A very large application, frontend is plain JavaScript/jQuery, backend is Perl. The views are made running XSLT transforms on a XML Document Database. I worked on adding new features and fixing bugs through the full stack.",
+        "imgurl":"images/portfolio/fierfoxpatientlist.png"
       },
       {
-        "name":"project2",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/project.jpg"
+        "name":"C2",
+        "description":"A smaller, mobile version of Hummingbird Clinical, uses the same technologies. I worked on adding new features and fixing bugs through the full stack.",  
+        "imgurl":"images/portfolio/C2patientchart.png"
       },
       {
-        "name":"project3",
-        "description":"mobileapp",  
-        "imgurl":"images/portfolio/project2.png"
+        "name":"ClearScripts",
+        "description":"A React Micro-app that is integrated with multiple 3rd party e-prescribing companies to displays medications for a patient entered in those systems. I created this from scratch, from the workflow diagram to deployed to production in 6 weeks.",
+        "imgurl":"images/portfolio/clearscripts.png"
       },
       {
-        "name":"project4",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
+        "name":"Scheduler",
+        "description":"A Angular app that handles scheduling recurring visits for a patient. I added minor features and did bug fixes.",
+        "imgurl":"images/portfolio/scheduler.png"
+      },
+      {
+        "name":"Certification App",
+        "description":"A React Micro-app that allows physicians to sign certifications. I added minor features and did bug fixes.",
+        "imgurl":"images/portfolio/certappcert.png"
       }
     ],
     "testimonials":[
       {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      },
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
+        "description":"I just want to let you know that I am very happy with your work. Getting C2 to the point where it can be put into production was a huge task. Creating the virtual dev server for HBC will be a big multiplier. Your willingness to jump into Perl and Apache config is commendable – the 10X speed improvement for our web apps will provide huge benefits to our end-users. You ask questions, quickly, instead of wasting hours searching for answers. You take feedback extremely well. Most importantly, you see things through to the end, producing working software for our clients.",
+        "name":"Steve Turoff - CTO"
       }
     ]
   }
