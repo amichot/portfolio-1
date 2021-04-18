72435b6b1d7fec9bdc4ca1097bcbcdd101b7ec6f
Adrian Michot <30712077+amichot@users.noreply.github.com>
2021-04-17 22:03:12 -0500

    Feat - Adrian's Portfolio

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,47 +1,47 @@
 let resumeData = {
     "imagebaseurl":"",
-    "name": "Jamie Keelen",
+    "name": "Adrian Michot",
     "role": "Full Stack Developer",
-    "linkedinId":"jamiekeelen",
+    "linkedinId":"amichot",
     "skypeid": "Your skypeid",
     "roleDescription": "I am a full-stack developer with a deep passion for creating amazing things. I specialize in all things JavaScript especially the MERN (Mongo Express React Node) Stack.",
     "socialLinks":[
         {
           "name":"linkedin",
-          "url":"https://www.linkedin.com/in/jamiekeelen/",
+          "url":"https://www.linkedin.com/in/amichot/",
           "className":"fa fa-linkedin"
         },
         {
           "name":"github",
-          "url":"http://github.com/jamie7893",
+          "url":"https://github.com/amichot/",
           "className":"fa fa-github"
         }
       ],
-    "aboutme":"Currently I am working full time as a Full Stack Developer for a software company in the healthcare space. I've been in my current position for 4+ years and I am ready to take the next step in my career. In 2016 I completed the Hack Reactor program, a 3 month, 6 days a week, 12+ hours a day, very intense deep dive into JavaScript and all the frameworks and technologies surrounding it.",
-    "address":"jamie78933@gmail.com",
-    "website":"(504) 952-3690",
+    "aboutme":"Currently I am working full time as a Full Stack Developer for a software company in the education/safety space. I've been in my current position for almost 2 years and I am ready to take the next step in my career. In 2019 I completed the Thinkful program, a 6 month, 5 days a week, 8+ hours a day, very intense deep dive into JavaScript and all the frameworks and technologies surrounding it.",
+    "address":"adrian_michot@yahoo.com",
+    "website":"(985) 414-4836",
     "education":[
+      
       {
-        "UniversityName":"Operation Spark",
-        "specialization":"Hack Reactor",
-        "MonthOfPassing":"Aug",
-        "YearOfPassing":"2016"
+        "UniversityName":"Thinkful",
+        "specialization":"Full Stack Flex",
+        "MonthOfPassing":"June",
+        "YearOfPassing":"2019"
+      },
+      {
+        "UniversityName":"Nicholls State University",
+        "specialization":"Humanities",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2014"
       }
     ],
     "work":[
       {
-        "CompanyName":"Mumms Software",
+        "CompanyName":"Alliance Safety Council",
         "specialization":"Full Stack Developer",
-        "MonthOfLeaving":"Sept 2016 - Current",
+        "MonthOfLeaving":"Aug 2019 - Current",
         "YearOfLeaving":"",
-      "Achievements":"Led the transition from a plain vanilla JavaScript/jQuery app to React components built as micro-services. Improved the performance of much of their existing stack by over 10x. Improved the quality of life for the rest of the development team by teaching them new technologies and streamlining the workflow process. Maintained existing software in Perl, Bash, XML, XSLT, JavaScript, and SQL."
-      },
-      {
-        "CompanyName":"The Refinery Haus",
-        "specialization":"Freelance React/Redux",
-        "MonthOfLeaving":"Feb",
-        "YearOfLeaving":"2020",
-        "Achievements": "Converted a existing Angular app to a React + Redux app"
+      "Achievements":"Leading the transition from a Visual Basic app to React components built as micro-services. Improved the performance of the existing stack. Improved the quality of life for the rest of the development team by helping them with new technologies and streamlining the workflow process. Maintained existing software in Visual Basic, JavaScript, and SQL."
       }
     ],
     "skillsDescription":"",
@@ -50,7 +50,7 @@ let resumeData = {
         "skillname":"JavaScript"
       },
       {
-        "skillname":"TypeScript"
+        "skillname":"Visual Basic"
       },
       {
         "skillname":"React"
@@ -62,7 +62,7 @@ let resumeData = {
         "skillname":"SQL"
       },
       {
-        "skillname":"Linux"
+        "skillname":"AWS"
       },
       {
         "skillname":"Bash"
@@ -70,35 +70,35 @@ let resumeData = {
     ],
     "portfolio":[
       {
-        "name":"Hummingbird Clinical",
-        "description":"A complete EMR (Electronic Medical Record) system for the hospice industry. The frontend consists of a 25,000 line JavaScript/jQuery application that transforms an XML data store to XHTML via XSLT Transforms. The backend is a collection of Perl, Bash, PSQL, XML, and XSLT comprising 30,000 lines of code.",
-        "imgurl":"images/portfolio/fierfoxpatientlist.png"
+        "name":"Student Reports",
+        "description":"A five report system for the user to view student data. The frontend consists of a JavaScript/React application that utilizes AG Grid to search and display large complex datasets. The backend is built in AWS using NodeJS and Microsoft SQL.",
+        "imgurl":"images/portfolio/StudentReports.jpg"
       },
       {
-        "name":"C2",
-        "description":"A smaller, mobile version of Hummingbird Clinical, uses the same technologies. I worked on adding new features and fixing bugs through the full stack.",  
-        "imgurl":"images/portfolio/C2patientchart.png"
+        "name":"Email Template Editor",
+        "description":"Uses custom variables to create and edit email templates for mass email sending to students.",  
+        "imgurl":"images/portfolio/EmailTemplateCreator.jpg"
       },
       {
-        "name":"ClearScripts",
-        "description":"A React Micro-app that is integrated with multiple 3rd party e-prescribing companies to displays medications for a patient entered in those systems. I created this from scratch, from the workflow diagram to deployed to production in 6 weeks.",
-        "imgurl":"images/portfolio/clearscripts.png"
+        "name":"Course Category Add Edit",
+        "description":"A complex category system that ultimately determines which categories show up for company websites. It is composed of Javascript/Jquery along with .Net",
+        "imgurl":"images/portfolio/CourseCategoryAddEdit.jpg"
       },
       {
-        "name":"Scheduler",
-        "description":"A Angular app that handles scheduling recurring visits for a patient. I added new features and did bug fixes.",
-        "imgurl":"images/portfolio/scheduler.png"
+        "name":"PhotoUploader",
+        "description":"A drag and drop photo uploader for users to upload profile pictures.",
+        "imgurl":"images/portfolio/PhotoUploader.jpg"
       },
       {
-        "name":"Certification App",
-        "description":"A React Micro-app that allows physicians to sign certifications. I added new features and did bug fixes.",
-        "imgurl":"images/portfolio/certappcert.png"
+        "name":"Student Check-In",
+        "description":"A service app used by front office to check students into classes. I worked on adding new features and fixing bugs through the full stack.",
+        "imgurl":"images/portfolio/StudentCheckIn.jpg"
       }
     ],
     "testimonials":[
       {
-        "description":"I just want to let you know that I am very happy with your work. Getting C2 to the point where it can be put into production was a huge task. Creating the virtual dev server for HBC will be a big multiplier. Your willingness to jump into Perl and Apache config is commendable – the 10X speed improvement for our web apps will provide huge benefits to our end-users. You ask questions, quickly, instead of wasting hours searching for answers. You take feedback extremely well. Most importantly, you see things through to the end, producing working software for our clients.",
-        "name":"Steve Turoff - CTO"
+        "description":"",
+        "name":""
       }
     ]
   }
