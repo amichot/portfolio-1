677ad750bc67da25c8f75afc58de0d5717532b1e
Adrian Michot <30712077+amichot@users.noreply.github.com>
2021-04-19 16:52:22 -0500

    chore: build

---
+++ build/precache-manifest.93f82bb28621a2a96080566ba56dcb23.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fabc3ec38d5d566657ec",
+    "url": "/static/css/main.02a71b4b.chunk.css"
+  },
+  {
+    "revision": "fabc3ec38d5d566657ec",
+    "url": "/static/js/main.fabc3ec3.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b5f5c61dad0dc0a5ef9b422cee60f0a5",
+    "url": "/index.html"
+  }
+];
